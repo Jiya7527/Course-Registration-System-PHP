f3f703e18ebe0de3616ba6333100a884cb4f7782
Jiya Sharma <sjiya7527@gmail.com>
2024-07-11 17:59:33 +0530

    Update onlinecourse.sql

--- Online Course Registration(PHP)/SQL File/onlinecourse.sql
+++ Online Course Registration(PHP)/SQL File/onlinecourse.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Apr 25, 2024 at 07:58 AM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
